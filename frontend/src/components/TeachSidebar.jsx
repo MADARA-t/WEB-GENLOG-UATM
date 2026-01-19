7f288b26139bac01a1c06576ad2dc29e7db0a811
MEDEGNON199 <huguesmedegnon842@gmail.com>
2026-01-19 08:27:25 +0100

    ajout

--- frontend/src/components/TeachSidebar.jsx
+++ frontend/src/components/TeachSidebar.jsx
@@ -7,6 +7,7 @@ const TeachSidebar = () => {
 
     const navigationLinks = [
   { icon: 'school', label: 'Espaces pédagogiques', path: '/formateur/espac' },
+  { icon: 'task', label: 'Gestion des travaux', path: '/formateur/travaux' },
 ];
 
     return (
